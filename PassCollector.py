56f94eb8fcdb4955545420b3d229c6f5378fc902
cjsiador <christiansiador@gmail.com>
2022-06-04 14:25:16 -1000

    Created Login

---
+++ PassCollector.py
@@ -0,0 +1,108 @@
+import sqlite3, hashlib
+from tkinter import *
+import customtkinter
+
+#Database Code
+with sqlite3.connect("password_vault.db") as db:
+    cursor = db.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS masterpassword(
+id INTEGER PRIMARY KEY,
+password TEXT NOT NULL);
+""") 
+
+#Initiate Window
+window = Tk()
+
+window.title("PassCollector")
+
+def hashPassword(input):
+    hash = hashlib.md5(input)
+    hash = hash.hexdigest()
+
+    return hash
+
+def firstScreen():
+    window.geometry("250x100")
+
+    lbl = Label(window, text="Create Master Password")
+    lbl.config(anchor=CENTER)
+    lbl.pack()
+
+    txt = Entry(window, width=20, show="*")
+    txt.pack()
+    txt.focus()
+
+    lbl1 = Label(window, text="Re-enter Password")
+    lbl1.pack()
+
+    txt1 = Entry(window, width=20)
+    txt1.pack()
+    txt1.focus()
+
+    lbl2 = Label(window)
+    lbl2.pack()
+
+    def savePassword():
+        if txt.get() == txt1.get():
+            hashedPassword = hashPassword(txt.get().encode('utf-8'))
+            
+            insert_password = """INSERT INTO masterpassword(password)
+            VALUES(?) """
+            cursor.execute(insert_password, [(hashedPassword)])
+            db.commit()
+
+            passwordVault()
+        else:
+            lbl2.config(text="Password Do Not Match")
+
+    btn = Button(window, text="Save", command=savePassword)
+    btn.pack(pady=10)
+
+def loginScreen():                                        
+    window.geometry("250x100")
+
+    lbl = Label(window, text="Enter Master Password")
+    lbl.config(anchor=CENTER)
+    lbl.pack()
+
+    txt = Entry(window, width=20, show="*")
+    txt.pack()
+    txt.focus()
+
+    lbl1 = Label(window)
+    lbl1.pack()
+
+    def getMasterPassword():
+        checkHashedPassword = hashPassword(txt.get().encode('utf-8'))
+        cursor.execute("SELECT * FROM masterpassword WHERE id = 1 AND password = ?", [(checkHashedPassword)])
+        return cursor.fetchall()
+
+    def checkPassword():
+        match = getMasterPassword()
+
+        if match:
+            passwordVault()
+        else:
+            txt.delete(0, 'end')
+            lbl1.config(text="Wrong Password")
+
+    btn = Button(window, text="Submit", command=checkPassword)
+    btn.pack(pady=10)
+
+def passwordVault():                                       
+    for widget in window.winfo_children():
+        widget.destroy()
+    window.geometry("700x350")
+
+    lbl = Label(window, text="Password Vault")
+    lbl.config(anchor=CENTER)
+    lbl.pack()
+
+cursor.execute("SELECT * FROM masterpassword")
+if cursor.fetchall():
+    loginScreen()
+else:
+    firstScreen()
+window.mainloop()
