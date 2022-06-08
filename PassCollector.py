8ef129c7f5ca3cb668a5201e88e6294d037b59bc
kevinq22 <70749270+kevinq22@users.noreply.github.com>
2022-06-08 23:24:41 +1000

    added basic functions

--- PassCollector.py
+++ PassCollector.py
@@ -1,6 +1,9 @@
+from os import remove
 import sqlite3, hashlib
 from tkinter import *
-import customtkinter
+from tkinter import simpledialog
+from functools import partial 
+#import customtkinter
 
 #Database Code
 with sqlite3.connect("password_vault.db") as db:
@@ -11,6 +14,20 @@ CREATE TABLE IF NOT EXISTS masterpassword(
 id INTEGER PRIMARY KEY,
 password TEXT NOT NULL);
 """) 
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS vault(
+id INTEGER PRIMARY KEY,
+website TEXT NOT NULL,
+username TEXT NOT NULL,
+password TEXT NOT NULL);
+""") 
+
+#Create the Popup
+def popUp(text):
+    answer = simpledialog.askstring("input string", text)
+    
+    return answer
 
 #Initiate Window
 window = Tk()
@@ -94,11 +111,69 @@ def loginScreen():
 def passwordVault():                                       
     for widget in window.winfo_children():
         widget.destroy()
-    window.geometry("700x350")
 
+    def addEntry():
+        text1 = "Website"
+        text2 = "Username"
+        text3 = "Password"
+
+        website = popUp(text1)
+        username = popUp(text2)
+        password = popUp(text3)
+
+        insert_fields = """INSERT INTO vault(website, username, password)
+        VALUES(?, ?, ?)"""
+
+        cursor.execute(insert_fields, (website,username, password)) 
+        db.commit()
+
+        passwordVault()
+
+    def removeEntry(input):
+        cursor.execute("DELETE FROM VAULT WHERE id = ?", (input,))
+        db.commit()
+
+        passwordVault()
+
+    window.geometry("700x350")
+    
     lbl = Label(window, text="Password Vault")
-    lbl.config(anchor=CENTER)
-    lbl.pack()
+    lbl.grid(column=1)
+
+    btn = Button(window, text="+", command=addEntry)
+    btn.grid(column=1, pady=10)
+
+    lbl = Label(window, text="Website")
+    lbl.grid(row=2, column=0, padx=80)
+    lbl = Label(window, text="Username")
+    lbl.grid(row=2, column=1, padx=80)
+    lbl = Label(window, text="Password")
+    lbl.grid(row=2, column=2, padx=80)
+
+    cursor.execute("SELECT * FROM vault")
+    if(cursor.fetchall() != None):
+        i = 0
+        while True:
+            cursor.execute("SELECT * FROM vault")
+            array = cursor.fetchall()
+            #website label
+            lbl = Label(window, text=(array[i][1]), font=("Helvetica",12))
+            lbl.grid(column=0, row=i+3)
+            #username label
+            lbl = Label(window, text=(array[i][2]), font=("Helvetica",12))
+            lbl.grid(column=1, row=i+3)
+            #password label
+            lbl = Label(window, text=(array[i][3]), font=("Helvetica",12))
+            lbl.grid(column=2, row=i+3)
+
+            btn = Button(window, text="Delete", command= partial(removeEntry, array[i][0]))
+            btn.grid(column=3, row=i+3, pady=10)
+
+            i = i+1
+
+            cursor.execute("SELECT * FROM vault")
+            if(len(cursor.fetchall()) <= i):
+                break
 
 cursor.execute("SELECT * FROM masterpassword")
 if cursor.fetchall():
